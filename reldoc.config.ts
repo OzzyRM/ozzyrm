8439868c5149b2df0be17cdb6149389d20c27328
inRiza <icon.firdaus@gmail.com>
2026-07-24 10:09:32 +0700

    refactor(config): update schema configuration to support multiple versions and ORM types for Prisma and Drizzle

--- reldoc.config.ts
+++ reldoc.config.ts
@@ -1,14 +1,41 @@
-import { defineConfig } from "@reldoc/core";
+import { defineProject } from "@reldoc/core";
 
-export default defineConfig({
-    orm: "prisma",
-    include: ["./packages/prisma/test/schema.prisma"],
-    output: "./web",
+export default defineProject({
+    output: "./web/schemas",
+    schemas: [
+        {
+            id: "schema-prisma-1-0-0",
+            file: "schema.prisma",
+            version: "1.0.0",
+            orm: "prisma",
+            include: ["./packages/prisma/test/schema.prisma"],
+        },
+        {
+            id: "schema-prisma-2-0-0",
+            file: "schema.prisma",
+            version: "2.0.0",
+            orm: "prisma",
+            include: ["./packages/prisma/test/schema-v2.prisma"],
+        },
+        {
+            id: "schema-prisma-3-0-0",
+            file: "schema.prisma",
+            version: "3.0.0",
+            orm: "prisma",
+            include: ["./packages/prisma/test/schema-v3.prisma"],
+        },
+        {
+            id: "schema-prisma-multi-1-0-0",
+            file: "multi-file",
+            version: "1.0.0",
+            orm: "prisma",
+            include: ["./packages/prisma/test/multi-file"],
+        },
+        {
+            id: "schema-drizzle-1-0-0",
+            file: "schema.ts",
+            orm: "drizzle",
+            include: ["./packages/drizzle/test/schema.ts"],
+        },
+    ],
 });
-
-// Switch to drizzle example:
-// export default defineConfig({
-//     orm: "drizzle",
-//     include: ["./examples/drizzle/schema.ts"],
-//     output: "./web",
-// });
